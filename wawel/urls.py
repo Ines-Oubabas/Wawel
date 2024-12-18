66cfd84188acb8e18b6822fede35fdda38212905
Ines <inesoubabas56@gmail.com>
2024-12-18 22:18:43 +0100

    wawel url

--- wawel/urls.py
+++ wawel/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('magasin.urls')),  # Inclure les routes de l'application "magasin"
 ]
+
+# Ajout pour servir les fichiers médias en mode développement
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
