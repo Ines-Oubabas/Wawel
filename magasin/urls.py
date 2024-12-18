4195d451889ffb7ff1ec535decb86a13859e7287
Ines <inesoubabas56@gmail.com>
2024-12-18 22:17:31 +0100

    url

--- magasin/urls.py
+++ magasin/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Pages principales
     path('', views.index, name='index'),  # Page d'accueil
-    path('about/', views.about, name='about'), # Page a propos 
-    path('produits/', views.produits, name='produits'),
+    path('about/', views.about, name='about'),  # Page À propos
+    path('produits/', views.produits, name='produits'),  # Page des produits
     path('contact/', views.contact, name='contact'),  # Page de contact
+    path('submit-contact/', views.submit_contact, name='submit_contact'),  # Soumission du formulaire
+    path('newsletter/', views.newsletter_subscription, name='newsletter'),
+    
 ]
